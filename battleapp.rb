98c666e819931b561540f27c312cfa157e69f54d
Sam Ellenrieder <sam.ell@web.de>
2017-08-18 12:24:38 +0100

    Battle Game, enter players

--- battleapp.rb
+++ battleapp.rb
@@ -1,12 +1,28 @@
-require  'sinatra'
+#This is the backend
+#I am the server in this file
+require  'sinatra' #require sinatra equals the whole sinnatra code. we run sinatra before we run our other code
 
-class Battle < Sinatra::Base
+class Battle < Sinatra::Base #require class Base in Sinatra because we want to work with that
 
-  get '/' do
-	"Testing infrastructure working!"
-  erb(:index)
+  get '/' do #if we get the get request with / we do this code
+	  "Testing infrastructure working!"
+    erb(:index) #we call index file / erb interprets html(index file syntax)(language)
+  end
+
+  post '/names' do
+    player_1_name = params[:player_1_name] || "human_1"
+    player_2_name = params[:player_2_name] || "human_2"
+    erb :names, :locals => {"player_1_name" => player_1_name, "player_2_name" => player_2_name} #different variable for the same value are called the same.
   end
 
 
 
+
+
 end
+
+
+
+
+#get request = we are the server. we get a get request and are asked to diplay code matching the get request.
+#post request = we are asked to take the values that were posted, send them to the backend, and work on them.
